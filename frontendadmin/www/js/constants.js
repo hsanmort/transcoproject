5e420917a9ccc27d7b56b850000845267587f298
adelcr <adel.chaabouni@sesame.com.tn>
2017-03-27 09:54:40 +0100

    admin view changed

--- frontendadmin/www/js/constants.js
+++ frontendadmin/www/js/constants.js
@@ -1,10 +1,3 @@
 angular.module('starter')
 
-.constant('AUTH_EVENTS', {
-  notAuthenticated: 'auth-not-authenticated'
-})
 
-.constant('API_ENDPOINT', {
-  url: 'http://127.0.0.1:3000/admin'
-  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
-});
